af262afe5d159235af64ab033eaa6372e782bc6e
victor pena <victor.pena1392@Gmail.com>
2022-03-24 20:00:30 -0500

    created an arraylist to capture each users response

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-$('#users-equation').keyup((e) => {
-    let equation = [];
-    let userInput = e.target.value;
-    $('#equals').click(function () {
-        equation.push(userInput);
-    });
+
+equationList = [];
+function calculateEquation() {
+    var equation = {
+        problem: ''
+    }
+    equation.problem = document.getElementById('users-equation').value;
+    equationList.push(equation);
     console.log(equation);
-});
+}
+
+var equalsBtn = document.getElementById('equals');
+equalsBtn.addEventListener('click', calculateEquation);
+
+// $('#equals').click(function () {
+//     let equation = {
+//         problem: ''
+//     }
+//     equation.problem = $('#users-equation').val;
+//     console.log(equation);
+// });
 
 
 //use konami code assignment as an example.
